3b4238a6bae528cbce8882e0b4dccf1f8ea3c33e
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-04-20 22:31:01 +0200

    button for opening the video drawing

--- TrueSwing-expo-app/features/analysis/components/AnalysisReelItem.tsx
+++ TrueSwing-expo-app/features/analysis/components/AnalysisReelItem.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
-import { Dimensions, View } from "react-native";
+import { Dimensions, View, Pressable } from "react-native";
 
 import type { Analysis } from "features/analysis/types";
 import useAnalysisData from "features/analysis/hooks/useAnalysisData";
 import Reel from "features/analysis/components/Reel";
 import IssueShowcaseOverlay from "features/analysis/components/IssueShowcaseOverlay";
 import { Issue } from "features/issues/types";
+import { Ruler } from "lucide-react-native";
+import { StyleSheet } from "react-native";
 
 const { height } = Dimensions.get("window");
 
@@ -50,6 +52,48 @@ export default function AnalysisReelItem({
                 onActiveIssueChange={onActiveIssueChange}
                 startPractice={startPractice}
             />
+
+            <View
+                pointerEvents="box-none"
+                style={StyleSheet.absoluteFill}
+            >
+                <Pressable
+                    onPress={() => console.log("Open video details")}
+                    style={{
+                        position: "absolute",
+                        right: 16,
+                        top: "42%",
+                        width: 48,
+                        height: 48,
+                        marginTop: -24,
+                        zIndex: 999,
+                        elevation: 999,
+                        borderColor: "rgba(255,255,255,0.1)",
+                        borderWidth: 2,
+                        borderRadius: 999,
+                    }}
+                >
+                    {({ pressed }) => (
+                        <View
+                            style={{
+                                flex: 1,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                borderRadius: 999,
+                                borderWidth: 1,
+                                borderColor: "rgba(255,255,255,0.1)",
+                                backgroundColor: pressed
+                                    ? "rgba(0,0,0,0.8)"
+                                    : "rgba(0,0,0,0.6)",
+                                transform: [{ scale: pressed ? 0.95 : 1 }],
+                            }}
+                        >
+                            <Ruler size={20} color="white" />
+                        </View>
+                    )}
+                </Pressable>
+            </View>
+
         </View>
     );
 }
