a58a99eb9151d4baaf435a19973806b77b51be06
Oskar Olofsson <oskarjolofsson@gmail.com>
2026-05-12 12:40:55 +0200

    fixed nav buttons layout on android

--- TrueSwing-expo-app/app/(app)/(tabs)/_layout.tsx
+++ TrueSwing-expo-app/app/(app)/(tabs)/_layout.tsx
@@ -1,9 +1,12 @@
 import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { Tabs } from "expo-router";
-import { Pressable, Text, View } from "react-native";
-import { Home, Upload, User, NotebookPen } from "lucide-react-native";
+import { Pressable, View } from "react-native";
+import { Home, Upload, User } from "lucide-react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 function SimpleTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
+    const insets = useSafeAreaInsets();
+
     return (
         <View
             style={{
@@ -11,8 +14,11 @@ function SimpleTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
                 borderTopWidth: 1,
                 borderTopColor: "#e5e7eb",
                 backgroundColor: "#121827",
-                paddingTop: 8,
-                paddingBottom: 30,
+
+                paddingTop: 0,
+                paddingBottom: Math.max(insets.bottom, 12),
+
+                height: 40 + Math.max(insets.bottom, 12),
             }}
         >
             {state.routes.map((route, index) => {
@@ -22,12 +28,11 @@ function SimpleTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
                 const color = isFocused ? "#ffffff" : "#6b7280";
                 const size = isFocused ? 27 : 25;
 
-                const icon =
-                    options.tabBarIcon?.({
-                        focused: isFocused,
-                        color,
-                        size,
-                    });
+                const icon = options.tabBarIcon?.({
+                    focused: isFocused,
+                    color,
+                    size,
+                });
 
                 const onPress = () => {
                     const event = navigation.emit({
@@ -63,7 +68,7 @@ function SimpleTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
 export default function TabsLayout() {
     return (
         <Tabs
-            screenOptions={{ 
+            screenOptions={{
                 headerShown: false,
             }}
             tabBar={(props) => <SimpleTabBar {...props} />}
@@ -72,24 +77,35 @@ export default function TabsLayout() {
                 name="upload"
                 options={{
                     title: "Upload",
-                    tabBarIcon: ({ color, size }) => <Upload color={color} size={size} />,
+                    tabBarIcon: ({ color, size }) => (
+                        <Upload color={color} size={size} />
+                    ),
                 }}
             />
-            <Tabs.Screen 
-                name="index" 
-                options={{ 
+
+            <Tabs.Screen
+                name="index"
+                options={{
                     title: "Analysis",
-                    tabBarIcon: ({ color, size }) => <Home color={color} size={size} />,
-                }}   
+                    tabBarIcon: ({ color, size }) => (
+                        <Home color={color} size={size} />
+                    ),
+                }}
             />
-            <Tabs.Screen 
-                name="profile" 
-                options={{ 
+
+            <Tabs.Screen
+                name="profile"
+                options={{
                     title: "Profile",
-                    tabBarIcon: ({ color, size, focused }) => <User color={color} size={size} strokeWidth={focused ? 3 : 2} />
-                }}   
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <User
+                            color={color}
+                            size={size}
+                            strokeWidth={focused ? 3 : 2}
+                        />
+                    ),
+                }}
             />
-            
         </Tabs>
     );
 }
